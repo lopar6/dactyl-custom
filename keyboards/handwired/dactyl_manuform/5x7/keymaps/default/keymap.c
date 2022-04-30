3b3a8d8e207772d886e7d63cc1480b541d7d9f2f
Logan Parker <lopar6@gmail.com>
2022-04-30 00:18:59 -0600

    thumb mapping changes

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
@@ -24,15 +24,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(_FN),        KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         TG(_GAMING),   KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V, KC_B,
         TO(_QWERTY),   KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,  TG(_FN),
-                                                    KC_BACKSPACE, KC_SPC,
-                                                    KC_LEFT_CTRL, KC_ENTER,
-                                                    KC_PAUSE, KC_LSFT,
+                                                    KC_LSFT, KC_SPC,
+                                                    KC_LEFT_ALT, KC_LEFT_CTRL,
+                                                    KC_PAUSE, KC_ENTER,
         // right hand
                           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,  KC_MINUS,   KC_EQUAL,
                           KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
                           KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_BACKSLASH,
                           KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_HOME, KC_PGUP,
-                                            KC_LEFT, KC_RGHT, KC_UP, KC_DOWN,  KC_PGDN,
+                                            KC_DOWN, KC_UP, KC_LEFT, KC_RGHT, KC_PGDN,
              KC_SPC, KC_BSPC,
              KC_ENTER, KC_RIGHT_SHIFT,
              KC_ESCAPE, KC_DELETE
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,       KC_G,    KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,
         _______,   KC_B,    KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V,
         _______,   KC_H,    KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,
-                                                    KC_LSFT, _______,
+                                                    KC_LEFT_CTRL, _______,
                                                     _______, _______,
                                                     _______, _______,
         // right hand
