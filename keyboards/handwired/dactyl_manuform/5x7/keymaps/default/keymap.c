2c2657b40522c6c19f6050d020313e3ce1f41266
Logan Parker <lopar6@gmail.com>
2022-05-02 00:16:54 -0600

    keymap update

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
@@ -20,22 +20,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         // Columns  "~"TO(_QWERTY)
         KC_ESC,         KC_GRAVE,    KC_1,    KC_2,   KC_3,   KC_4,   KC_5,
-        TG(_NUMPAD),    KC_TAB,      KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,
-        TG(_FN),        KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
+        KC_F2,    KC_TAB,      KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,
+        KC_F5,        KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         TG(_GAMING),   KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V, KC_B,
-        TO(_QWERTY),   KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,  TG(_FN),
+        TO(_QWERTY),   KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,  LT(_FN, KC_NO), //KC_TRNS ALL over for this?
                                                     KC_LSFT, KC_SPC,
-                                                    KC_LEFT_ALT, KC_LEFT_CTRL,
-                                                    KC_PAUSE, KC_ENTER,
+                                                    KC_LEFT_CTRL, KC_LEFT_ALT,
+                                                    KC_TAB, KC_ENTER,
         // right hand
                           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,  KC_MINUS,   KC_EQUAL,
-                          KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
+                          KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LEFT_BRACKET, KC_RBRC,
                           KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_BACKSLASH,
-                          KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_HOME, KC_PGUP,
-                                            KC_DOWN, KC_UP, KC_LEFT, KC_RGHT, KC_PGDN,
+                          KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP, KC_PSCR,
+                                            KC_HOME,  KC_END,   KC_LEFT,  KC_DOWN, KC_RGHT,
+
              KC_SPC, KC_BSPC,
-             KC_ENTER, KC_RIGHT_SHIFT,
-             KC_ESCAPE, KC_DELETE
+             KC_ENTER, KC_DEL, // TODO fix these keys are rotated
+             KC_PGDN, KC_PGUP
     ),
 
     [_GAMING] = LAYOUT_5x7(
