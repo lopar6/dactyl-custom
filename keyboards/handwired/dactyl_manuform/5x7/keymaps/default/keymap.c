2a8a32fc71ff2a6cdcd267daab5a6caafd8b11bd
Logan Parker <lopar6@gmail.com>
2022-07-23 20:07:55 -0600

    remap left thumb and gaming config

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
@@ -22,11 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_GRAVE,    KC_1,    KC_2,   KC_3,   KC_4,   KC_5,
         KC_F2,    KC_TAB,      KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,
         KC_F5,        KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
-        TG(_GAMING),   KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V, KC_B,
+        TO(_GAMING),   KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V, KC_B,
         TO(_QWERTY),   KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,  LT(_FN, KC_NO), //KC_TRNS ALL over for this?
-                                                    KC_LSFT, KC_SPC,
-                                                    KC_LEFT_CTRL, KC_LEFT_ALT,
-                                                    KC_TAB, KC_ENTER,
+                                                    KC_LSFT, KC_LEFT_CTRL,
+                                                    KC_LEFT_ALT, KC_TAB,
+                                                    KC_BSPC, KC_ENTER,
         // right hand
                           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,  KC_MINUS,   KC_EQUAL,
                           KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LEFT_BRACKET, KC_RBRC,
@@ -35,18 +35,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_HOME,  KC_END,   KC_LEFT,  KC_DOWN, KC_RGHT,
 
              KC_SPC, KC_BSPC,
-             KC_ENTER, KC_DEL, // TODO fix these keys are rotated
+             KC_ENTER, KC_DEL,
              KC_PGDN, KC_PGUP
     ),
-
+// todo update gaming
     [_GAMING] = LAYOUT_5x7(
         // left hand
         // Columns  "~"
         KC_ESC,        _______,       KC_GRAVE,     KC_1,    KC_2,   KC_3,   KC_4,
         _______,   KC_T,       KC_TAB,       KC_Q,    KC_W,   KC_E,   KC_R,
-        _______,       KC_G,    KC_CAPS,      KC_A,    KC_S,   KC_D,   KC_F,
-        _______,   KC_B,    KC_LSFT,      KC_Z,    KC_X,   KC_C,   KC_V,
-        _______,   KC_H,    KC_LEFT_CTRL, KC_LGUI,   KC_LEFT_ALT,
+        _______,       KC_G,    KC_LSFT,      KC_A,    KC_S,   KC_D,   KC_F,
+        _______,   KC_B,    KC_LEFT_CTRL,      KC_Z,    KC_X,   KC_C,   KC_V,
+        _______,   KC_H,    KC_LGUI, KC_LEFT_ALT,   KC_G,
                                                     KC_LEFT_CTRL, _______,
                                                     _______, _______,
                                                     _______, _______,
